4e868eeb5b8f31df57e67b9937b0740f6e9c7e7d
gabrielmokayed <gabriel.mokayed@gmail.com>
2025-03-08 23:43:27 +0100

    Create reveal.js

---
+++ netlify/functions/reveal.js
@@ -0,0 +1,23 @@
+exports.handler = async (event) => {
+  if (event.httpMethod === 'POST') {
+    const { buttonIndex } = JSON.parse(event.body);
+    const destinations = [
+      "https://static.wixstatic.com/media/5443c9_114aa225fdc4451a8325c1f302f23793~mv2.jpg/v1/fit/w_456,h_454,q_90,enc_avif,quality_auto/5443c9_114aa225fdc4451a8325c1f302f23793~mv2.jpg",
+      "https://static.wixstatic.com/media/5443c9_8f1e33151df344ddad6d779dd85c88f5~mv2.jpg/v1/fit/w_454,h_454,q_90,enc_avif,quality_auto/5443c9_8f1e33151df344ddad6d779dd85c88f5~mv2.jpg",
+    ];
+    const randomImageUrl = destinations[Math.floor(Math.random() * destinations.length)];
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: 'frame',
+        image: randomImageUrl,
+        buttons: [
+          { label: 'Reveal Destination', action: 'post' },
+          { label: 'Get Yours', action: 'link', target: 'https://opensea.io/collection/flymeta' },
+        ],
+      }),
+    };
+  }
+  return { statusCode: 405, body: 'Method not allowed' };
+};
