096259474ca970c7bab7de456a232cf51fecef3e
gabrielmokayed <gabriel.mokayed@gmail.com>
2025-03-09 11:02:58 +0100

    Update reveal.js

--- netlify/functions/reveal.js
+++ netlify/functions/reveal.js
@@ -17,7 +17,7 @@ export const handler = async (event) => {
       const jsonData = JSON.parse(data);
       console.log("Parsed JSON data:", jsonData); // Debug log
 
-      const destinations = jsonData.data.images; 
+      const destinations = jsonData.data.images; //
       console.log("Destinations array:", destinations); // Debug log
 
       const randomImageUrl = destinations[Math.floor(Math.random() * destinations.length)];
