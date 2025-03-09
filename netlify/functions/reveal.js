2b974b4bd0c9213c1235713c6e2d21c08621fac8
gabrielmokayed <gabriel.mokayed@gmail.com>
2025-03-09 01:19:11 +0100

    Update reveal.js

--- netlify/functions/reveal.js
+++ netlify/functions/reveal.js
@@ -1,18 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.handler = async (event) => {
   if (event.httpMethod === 'POST') {
     const body = event.body ? JSON.parse(event.body) : {};
 
-    const destinations = [
-      "https://static.wixstatic.com/media/5443c9_114aa225fdc4451a8325c1f302f23793~mv2.jpg",
-      "https://static.wixstatic.com/media/5443c9_8f1e33151df344ddad6d779dd85c88f5~mv2.jpg"
-    ];
-    
+    // Load destinations from JSON file
+    const jsonPath = path.join(__dirname, 'destinations.json');
+    const data = fs.readFileSync(jsonPath, 'utf8');
+    const destinations = JSON.parse(data).destinations;
+
+    // Select a random image URL
     const randomImageUrl = destinations[Math.floor(Math.random() * destinations.length)];
 
     return {
       statusCode: 200,
       headers: { 
-        "Content-Type": "application/json; charset=utf-8",  // 🔥 Enforce JSON response
+        "Content-Type": "application/json; charset=utf-8",
         "Access-Control-Allow-Origin": "*"
       },
       body: JSON.stringify({
